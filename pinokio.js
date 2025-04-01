653787294c7805eed7f269310eadd87bb18963c0
6Morpheus6 <wanton-decay@gmx.net>
2025-04-01 03:07:35 +0200

    use info, add update and reset button

--- pinokio.js
+++ pinokio.js
@@ -1,41 +1,86 @@
-const os = require('os')
-const fs = require('fs')
 const path = require("path")
-const exists = (filepath) => {
-  return new Promise(r=>fs.access(filepath, fs.constants.F_OK, e => r(!e)))
-}
 module.exports = {
+  version: "3.0",
   title: "XTTS",
   description: "clone voices into different languages by using just a quick 3-second audio clip. (a local version of https://huggingface.co/spaces/coqui/xtts)",
   icon: "icon.png",
-  menu: async (kernel) => {
-    let installed = await exists(path.resolve(__dirname, "env"))
-    if (installed) {
-      let session = (await kernel.loader.load(path.resolve(__dirname, "session.json"))).resolved
+  menu: async (kernel, info) => {
+    let installed = info.exists("env")
+    let running = {
+      install: info.running("install.json"),
+      start: info.running("start.json"),
+      update: info.running("update.js"),
+      reset: info.running("reset.js")
+    }
+    if (running.install) {
       return [{
-        when: "start.json",
-        on: "<i class='fa-solid fa-spin fa-circle-notch'></i> Running",
-        type: "label",
-        href: "start.json"
-      }, {
-        when: "start.json",
-        off: "<i class='fa-solid fa-power-off'></i> Launch",
-        href: "start.json?fullscreen=true&run=true",
-      }, {
-        when: "start.json",
-        on: (session && session.url ? "<i class='fa-solid fa-rocket'></i> Open Web UI" : null),
-        href: (session && session.url ? session.url : null),
-        target: "_blank"
-      }, {
-        when: "start.json",
-        on: "<i class='fa-solid fa-desktop'></i> Server",
-        href: "start.json?fullscreen=true"
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.json",
       }]
+    } else if (running.update) {
+      return [{
+        default: true,
+        icon: 'fa-solid fa-terminal',
+        text: "Updating",
+        href: "update.js",
+      }]
+    } else if (installed) {
+      if (running.start) {
+        let local = info.local("start.json")
+        if (local && local.url) {
+          return [{
+            default: true,
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.json",
+          }]
+        } else {
+          return [{
+            default: true,
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.json",
+          }]
+        }
+      } else if (running.reset) {
+          return [{
+            default: true,
+            icon: 'fa-solid fa-terminal',
+            text: "Resetting",
+            href: "reset.js",
+          }]
+      } else {
+        return [{
+          default: true,
+          icon: "fa-solid fa-power-off",
+          text: "Start",
+          href: "start.json",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.json",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "Reset",
+          href: "reset.js",
+        }]
+      }
     } else {
       return [{
-        html: '<i class="fa-solid fa-plug"></i> Install',
-        type: "link",
-        href: "install.json?run=true&fullscreen=true"
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.json",
       }]
     }
   }
