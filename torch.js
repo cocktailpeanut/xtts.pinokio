1237d22a231017c46874f96ed573820f5c77124c
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-04-23 22:50:49 +0200

    stable torch

--- torch.js
+++ torch.js
@@ -1,13 +1,13 @@
 module.exports = {
   run: [
-    // nvidia 50 series (windows)
+    // nvidia 50 series
     {
       "when": "{{gpu === 'nvidia' && kernel.gpu_model && / 50.+/.test(kernel.gpu_model) }}",
       "method": "shell.run",
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
+        "message": "uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128"
       },
       "next": null
     },
